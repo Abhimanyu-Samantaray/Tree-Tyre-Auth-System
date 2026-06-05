4321fa048bec9070ce0f366c06786246f778aef9
Abhimanyu-Samantaray <ass.samantaray@gmail.com>
2026-06-05 14:19:54 +0530

    Commit Three Tyre Auth System

---
+++ src/main/java/com/__tire_auth/Three_Tyre_Auth_System/ThreeTyreAuthSystemApplication.java
@@ -0,0 +1,13 @@
+package com.__tire_auth.Three_Tyre_Auth_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeTyreAuthSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreeTyreAuthSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/__tire_auth/Three_Tyre_Auth_System/ThreeTyreAuthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.__tire_auth.Three_Tyre_Auth_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeTyreAuthSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
